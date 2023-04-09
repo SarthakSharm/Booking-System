4b394bb2a6ac56b7ab7c9b94c7f33966251daaba
Shreyash Srivastava <shreyash.rocks.ever.10231@gmail.com>
2023-04-09 15:35:34 +0530

    auth

--- accounts/auth.py
+++ accounts/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from datetime import datetime
 from website import db
@@ -6,9 +8,24 @@ from website import db
 auth = Blueprint("auth", __name__, url_prefix="/auth")
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    return "<p>Login</p>"
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully!", category="success")
+                login_user(user, remember=True)
+                return redirect(url_for("views.home"))
+            else:
+                flash("Incorrect password, try again.", category="error")
+        else:
+            flash("Email does not exist.", category="error")
+
+    return render_template("login.html", user=current_user)
 
 
 @auth.route("/logout")
@@ -22,13 +39,12 @@ def signup():
         # Handle GET request
         return render_template("signup.html")
     if request.method == "POST":
-
         new_user = User(
-            name = request.form.get('Name'),
-            email = request.form.get('Email'),
-            dob = datetime.strptime(request.form.get("Dob"), "%Y-%m-%d"),
-            phone = request.form.get('Phone'),
-            password = request.form.get('Password'),
+            name=request.form.get("Name"),
+            email=request.form.get("Email"),
+            dob=datetime.strptime(request.form.get("Dob"), "%Y-%m-%d"),
+            phone=request.form.get("Phone"),
+            password=request.form.get("Password"),
         )
         db.session.add(new_user)
         db.session.commit()
