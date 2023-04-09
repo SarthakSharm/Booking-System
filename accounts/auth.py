dd87533d5b35ad09869f098ea73049575074d781
Shreyash Srivastava <shreyash.rocks.ever.10231@gmail.com>
2023-04-09 16:09:59 +0530

    Updated templates, added login

--- accounts/auth.py
+++ accounts/auth.py
@@ -37,16 +37,16 @@ def logout():
 def signup():
     if request.method == "GET":
         # Handle GET request
-        return render_template("signup.html")
+        return render_template("signup.html", user=current_user)
     if request.method == "POST":
         new_user = User(
             name=request.form.get("Name"),
             email=request.form.get("Email"),
             dob=datetime.strptime(request.form.get("Dob"), "%Y-%m-%d"),
             phone=request.form.get("Phone"),
-            password=request.form.get("Password"),
+            password=generate_password_hash(request.form.get("Password")),
         )
         db.session.add(new_user)
         db.session.commit()
-        return render_template("signup.html")
+        return render_template("signup.html", user=current_user)
     return "Invalid method"
