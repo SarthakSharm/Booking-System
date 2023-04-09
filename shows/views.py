65e39c296e212ef4630337e66731b2ce3f30a490
Sarthak Sharma <sarthaksharma070@gmail.com>
2023-04-09 21:08:27 +0530

    admin powers given

--- shows/views.py
+++ shows/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, redirect, url_for
+from flask import Blueprint, flash, request, render_template, redirect, url_for
 from .models import Show
 from datetime import datetime
 from flask_login import login_user, login_required, logout_user, current_user
@@ -15,20 +15,50 @@ def shows():
         return render_template("show.html", shows=shows, user=current_user)
 
 
+@show.route("/delete", methods=["GET", "POST"])
+def delete():
+    if current_user.is_admin == False:
+        flash("You are not admin", category="error")
+        return redirect(url_for("show.shows"))
+    
+    if request.method == "POST":
+        if current_user.is_admin == True:
+            show = Show(
+                name=request.form.get("name"),
+                tag=request.form.get("tag"),
+                rating=request.form.get("rating"),
+                ticket_price=request.form.get("price"),
+                venue_id=request.form.get("venue"),
+                date=datetime.strptime(request.form.get("date"), "%Y-%m-%d"),
+            )
+            db.session.add(show)
+            db.session.commit()
+            return redirect(url_for("show.shows"))
+
+    if request.method == "GET":
+        venues = Venue.query.all()
+        return render_template("createShow.html", user=current_user, venues=venues)
+    
+
 @show.route("/create", methods=["GET", "POST"])
 def create():
-    if request.method == "POST":
-        show = Show(
-            name=request.form.get("name"),
-            tag=request.form.get("tag"),
-            rating=request.form.get("rating"),
-            ticket_price=request.form.get("price"),
-            venue_id=request.form.get("venue"),
-            date=datetime.strptime(request.form.get("date"), "%Y-%m-%d"),
-        )
-        db.session.add(show)
-        db.session.commit()
+    if current_user.is_admin == False:
+        flash("You are not admin", category="error")
         return redirect(url_for("show.shows"))
+    
+    if request.method == "POST":        
+            show = Show(
+                name=request.form.get("name"),
+                tag=request.form.get("tag"),
+                rating=request.form.get("rating"),
+                ticket_price=request.form.get("price"),
+                venue_id=request.form.get("venue"),
+                date=datetime.strptime(request.form.get("date"), "%Y-%m-%d"),
+            )
+            db.session.add(show)
+            db.session.commit()
+            return redirect(url_for("show.shows"))
+
     if request.method == "GET":
         venues = Venue.query.all()
         return render_template("createShow.html", user=current_user, venues=venues)
--- venue/views.py
+++ venue/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, redirect, url_for
+from flask import Blueprint, flash, request, render_template, redirect, url_for
 from .models import Venue
 from flask_login import login_user, login_required, logout_user, current_user
 from datetime import datetime
@@ -9,7 +9,6 @@ venue = Blueprint("venue", __name__, url_prefix="/venue")
 
 
 @venue.route("/", methods=["GET"])
-@login_required
 def venues():
     if request.method == "GET":
         venues = Venue.query.all()
@@ -18,15 +17,21 @@ def venues():
 
 @venue.route("/create", methods=["GET", "POST"])
 def create():
+    if current_user.is_admin == False:
+        flash("You are not admin", category="error")
+        return redirect(url_for('venue.venues'))
+    
     if request.method == "POST":
-        venue = Venue(
-            name=request.form.get("name"),
-            place=request.form.get("place"),
-            capacity=request.form.get("capacity"),
-            screens=request.form.get("screens"),
-        )
-        db.session.add(venue)
-        db.session.commit()
-        return redirect(url_for("venue.venues"))
+        if current_user.is_admin == True:
+            venue = Venue(
+                name=request.form.get("name"),
+                place=request.form.get("place"),
+                capacity=request.form.get("capacity"),
+                screens=request.form.get("screens"),
+            )
+            db.session.add(venue)
+            db.session.commit()
+            return redirect(url_for('venue.venues'))
+       
     if request.method == "GET":
         return render_template("createVenue.html", user=current_user)
